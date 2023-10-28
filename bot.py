b71dc663bd988ad9b61d4211828f21890789fa95
sb171732 <sauronkan@gmail.com>
2023-10-28 15:20:12 +0700

    get user id

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ bot = telebot.TeleBot("6570762751:AAEwozETUMOVUP81VK4FAZwwRcN6N86Aw_w", parse_mo
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-
+	# bot.reply_to(message, "Howdy, how are you doing?")
+    print(message.from_user.id)
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
-
+    
 bot.infinity_polling()
